7bc43b8b157f081b20a587d4981e36f463cad359
Bruno dos Santos <bruno.coelho.santos17@gmail.com>
2023-10-21 12:07:36 -0300

    first commit

---
+++ src/main/java/com/contato/lista/Lista_Contatos/ListaContatosApplication.java
@@ -0,0 +1,13 @@
+package com.contato.lista.Lista_Contatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaContatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaContatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contato/lista/Lista_Contatos/ListaContatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contato.lista.Lista_Contatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaContatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
